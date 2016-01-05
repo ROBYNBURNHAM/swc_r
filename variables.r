19a10528752416e21fc7765fd5d17911c38b5257
Robyn Burnham <rburnham@umich.edu>
2016-01-05 11:23:55 -0500

    adding commented header

---
+++ variables.r
@@ -0,0 +1,25 @@
+# This is the document where I learn introductory R.
+x<-1 #
+# start lines with hashes to separate sections
+y<-x*x
+z<-y*y   # this would not execute
+
+x+y+z #
+mass <- 47.5
+age <- 122
+mass <- mass * 2.3
+age <- age - 20
+ratio<-mass/age
+as.logical(answer)
+logical(length=10)
+as.logical(answer)
+answer<-TRUE
+is.logical(answer)
+height<-15
+typeof(height)
+dog_name<-'Puppy'
+typeof(dog_name)
+
+xx <- c(1.7, "a")
+as.numericxx <- c(TRUE, 2)
+xx <- c("a", TRUE)
